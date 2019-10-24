9e63955f210cffc65a308b9588223519883e3e74
Habib Basher <hub0904010@gmail.com>
2019-10-24 23:27:43 +0600

    another test case for test

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,3 +1,8 @@
 def add(x, y):
-  """Add two number together"""
-  return x + y;
+    """Add two number together"""
+    return x + y
+
+
+def subtract(x, y):
+    """Add two number together"""
+    return y - x
